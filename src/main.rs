45f0217ee890c1ce6173fb371fd397f39b1c78ba
Leo Meira Vital <leo.vital@nubank.com.br>
2020-06-07 23:43:12 -0300

    Properly getting LOCAL and REMOTE

--- src/main.rs
+++ src/main.rs
@@ -1,22 +1,34 @@
+// To be used by git-difftool using extcmd flag
+
 use serde_json::{Value};
-use text_io::read;
+use std::env;
 use std::error::Error;
+use std::io::prelude::*;
 use std::fs::File;
 use std::io::BufReader;
 
-fn read_json(file_path : &str) -> Result<(), Box<dyn Error>> {
-    let file = File::open(file_path)?;
-    let reader = BufReader::new(file);
-    let v: Value = serde_json::from_reader(reader)?;
+// TODO Verify type of file in order to get diff
 
-    println!("a: {}, b: {}", v["a"], v["b"]);
+fn read_json(local_path : &str, remote_path : &str) -> Result<(), Box<dyn Error>> {
+    let local_file = File::open(local_path)?;
+    let local_reader = BufReader::new(local_file);
+
+    let remote_file = File::open(remote_path)?;
+    let remote_reader = BufReader::new(remote_file);
+
+    // let v: Value = serde_json::from_reader(reader)?;
 
     Ok(())
 }
 
 fn main() -> Result<(), Box<dyn Error>> {
-    let file_path : String = read!();
-    read_json(&file_path)?;
+    let args: Vec<String> = env::args().collect();
+    let local_path = &args[1];
+    let remote_path = &args[2];
+    let base = env::var("BASE")?;
+    println!("{}", base);
+    
+    read_json(local_path, remote_path)?;
 
     Ok(())
 }
