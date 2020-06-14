e8a007e0b0d45936d36f47a56f15d4f0807fcec7
Leo Meira Vital <leo.vital@nubank.com.br>
2020-06-14 20:09:56 -0300

    Implementing the main functionality of JSON diff.

--- src/formats/json.rs
+++ src/formats/json.rs
@@ -33,21 +33,25 @@ fn get_diff_color(origin: &str) -> Result<ansi_term::Colour, Box<dyn Error>> {
 fn get_formatted_diff<'a>(
     origin: &'a str,
     path: &'a Vec<String>,
+    values: Option<&Value>,
 ) -> Result<ansi_term::ANSIString<'a>, Box<dyn Error>> {
     let diff_symbol = get_diff_symbol(origin)?;
     let diff_color = get_diff_color(origin)?;
 
-    Ok(diff_color.paint(format!("{} {:?}", diff_symbol, path)))
+    match values {
+        Some(v) => Ok(diff_color.paint(format!("{} {:?} {}", diff_symbol, path, v))),
+        None => Ok(diff_color.paint(format!("{} {:?}", diff_symbol, path))),
+    }
 }
 
 fn print_missing_key(origin: &str, path: &Vec<String>) -> () {
-    match get_formatted_diff(origin, path) {
+    match get_formatted_diff(origin, path, None) {
         Ok(diff) => println!("{}", diff),
         Err(e) => println!("Error found when calculating diff: {}", e),
     }
 }
 
-fn recur(
+fn find_missing_keys(
     m1: &Map<String, Value>,
     m2: &Map<String, Value>,
     path: &Vec<String>,
@@ -56,32 +60,59 @@ fn recur(
     let mut path_copy = path.to_vec();
     for (k, v) in m1 {
         path_copy.push(k.to_string());
-        if v.is_object() {
-            if !m2.contains_key(k) {
-                print_missing_key(&m1_origin, &path_copy);
-            } else if !m2[k].is_object() {
-                //TODO print_changed_type(...)
-            } else {
-                recur(
-                    m1[k].as_object().unwrap(),
-                    m2[k].as_object().unwrap(),
-                    &path_copy,
-                    m1_origin,
+        if !m2.contains_key(k) {
+            print_missing_key(&m1_origin, &path_copy);
+        } else if v.is_object() && m2[k].is_object() {
+            find_missing_keys(
+                m1[k].as_object().unwrap(),
+                m2[k].as_object().unwrap(),
+                &path_copy,
+                m1_origin,
+            );
+        }
+    }
+}
+
+fn print_changed_value(path: &Vec<String>, local: &Value, remote: &Value) -> () {
+    match get_formatted_diff(LOCAL, path, Some(local)) {
+        Ok(diff) => println!("{}", diff),
+        Err(e) => println!("Error found when calculating diff: {}", e),
+    }
+    match get_formatted_diff(REMOTE, path, Some(remote)) {
+        Ok(diff) => println!("{}", diff),
+        Err(e) => println!("Error found when calculating diff: {}", e),
+    }
+}
+
+fn find_changed_values(
+    local: &Map<String, Value>,
+    remote: &Map<String, Value>,
+    path: &Vec<String>,
+) -> () {
+    for (k, local_v) in local {
+        if remote.contains_key(k) {
+            let remote_v = &remote[k];
+            if local_v.is_object() && remote_v.is_object() {
+                let mut path_copy_nested = path.to_vec();
+                path_copy_nested.push(k.to_string());
+                find_changed_values(
+                    local_v.as_object().unwrap(),
+                    remote_v.as_object().unwrap(),
+                    &path_copy_nested,
                 );
-            }
-        } else {
-            if !m2.contains_key(k) {
-                print_missing_key(&m1_origin, &path_copy);
-            } else {
-                // TODO compare values in m1[k] and m2[k], if different print_changed_type(...)
+            } else if *local_v != remote[k] {
+                let mut path_copy = path.to_vec();
+                path_copy.push(k.to_string());
+                print_changed_value(&path_copy, local_v, &remote[k]);
             }
         }
     }
 }
 
 fn calculate_diff(json1: &Map<String, Value>, json2: &Map<String, Value>) -> () {
-    recur(json1, json2, &Vec::new(), LOCAL);
-    recur(json2, json1, &Vec::new(), REMOTE);
+    find_missing_keys(json1, json2, &Vec::new(), LOCAL);
+    find_missing_keys(json2, json1, &Vec::new(), REMOTE);
+    find_changed_values(json1, json2, &Vec::new());
 }
 
 // TODO better error handling when JSON file has invalid JSON
